bc3b192a3f733edc6b03259008f24f273e892196
maxm <maxm@uubee.com>
2017-11-10 16:34:03 +0800

    修改spring mvc 配置

---
+++ src/main/java/com/mxm/java/learn_demo/controller/DemoController.java
@@ -0,0 +1,25 @@
+/**   
+* @Title: TestController.java 
+* @Package com.mxm.java.learn_demo.controller  
+* @author maxm@uubee.com  
+* @date 2017年11月10日 下午2:15:10 
+* @version V1.0   
+*/
+package com.mxm.java.learn_demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**  
+ * @author maxm@uubee.com
+ * @date 2017年11月10日 下午2:15:10 
+ */
+@Controller
+@RequestMapping("demo")
+public class DemoController {
+	
+	@RequestMapping("index")
+	public String demo(){
+		return "/index.jsp";
+	}
+}
