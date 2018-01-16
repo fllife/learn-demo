e24ca9c5b94c1224bd1e047bba479ef31acefa7d
maxianming <maxianming@dianwoba.com>
2018-01-16 16:47:44 +0800

    测试

--- src/main/java/com/mxm/java/learn_demo/App.java
+++ src/main/java/com/mxm/java/learn_demo/App.java
@@ -1,9 +1,7 @@
 package com.mxm.java.learn_demo;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.SynchronousQueue;
 
 
@@ -16,8 +14,8 @@ public class App
 	private static ThreadLocal<String> local = new ThreadLocal<String>();
     public static void main( String[] args ) throws Exception
     {   
-    	Map<String, String> map = new ConcurrentHashMap()<String, String>();
-    	map.size()
+    	Map<String, String> map = new ConcurrentHashMap<String, String>();
+    	map.size();
     	local.set("123");
     	local.get();
         System.out.println( "Hello World!" );
